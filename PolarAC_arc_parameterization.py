23350fbe496c44e300ce8c601cb5287fd9cfe117
grha6459 <97565231+grha6459@users.noreply.github.com>
2023-09-05 13:30:16 -0600

    Adding files

---
+++ PolarAC_arc_parameterization.py
@@ -0,0 +1,203 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 11 14:30:11 2023
+
+@author: graci
+"""
+
+import numpy as np
+import pandas as pd
+import os
+import time
+import glob
+
+startTime = time.time()
+
+# updated and corrected
+def arc_length(out_pd1):
+    distance = 0
+    for i in range(len(out_pd1)-1):
+        distance += ((out_pd1['x'][i]-out_pd1['x'][i+1])**2+(out_pd1['y'][i]-out_pd1['y'][i+1])**2)**0.5
+    distance_progress = 0
+    for i in range(len(out_pd1)-1):
+        distance_progress += ((out_pd1['x'][i]-out_pd1['x'][i+1])**2+(out_pd1['y'][i]-out_pd1['y'][i+1])**2)**0.5
+        #out_pd1['arc_length'][i+1] = distance_progress/distance*360
+        out_pd1.iloc[i,3] = distance_progress/distance*360
+        #print(distance_progress/distance*360)
+    #out_pd1['arc_length'][len(out_pd1)-1] = 360
+    #print(distance,distance_progress)
+    return
+
+def autocorrelate(vector):
+    denominator = 0
+    numerator = 0
+    result = [0]*len(vector)
+    for i in range(len(vector)):
+       denominator += (vector.iloc[i]-np.mean(vector))**2
+    for k in range(len(vector)):
+        for i in range(len(vector)-k):
+            numerator += (vector.iloc[i]-np.mean(vector))*(vector.iloc[i+k]-np.mean(vector))
+        result[k]=numerator/denominator
+        numerator=0
+    return result
+
+def sign_change(list_):
+    sign_changes = 0
+    for i in range(len(list_)-1):
+        if list_[i]*list_[i+1]<=0:
+            sign_changes=sign_changes+1
+    return sign_changes
+
+def tiebreaker(dataset):
+    derivative = []
+    second_derivative = []
+    for i in range(len(dataset)-1):
+        dx = dataset[i+1]-dataset[i]
+        derivative.append(dx)
+    for i in range(len(derivative)-1):
+        d2x = derivative[i+1]-derivative[i]
+        second_derivative.append(d2x)
+    return sign_change(second_derivative)
+
+def return_two_closest(point_,points_list):
+    x, y = point_[0], point_[1]
+    # gets smallest distance between point and minimum distance point
+    min_dist = min([((x-s[0])**2+(y-s[1])**2)**0.5 for s in points_list])
+    # first 
+    min_index = points_list.index([s for s in points_list if ((x-s[0])**2+(y-s[1])**2)**0.5==min_dist][0])
+    point1 = points_list[min_index]
+    # removes the point from both the local and global variable points list
+    points_list.remove(point1)
+    min_dist = min([((x-s[0])**2+(y-s[1])**2)**0.5 for s in points_list])
+    # gets index of point with the minimum distance 
+    min_index = points_list.index([s for s in points_list if ((x-s[0])**2+(y-s[1])**2)**0.5==min_dist][0])
+    point2 = points_list[min_index]
+    points_list.remove(point2)
+    return point1, point2
+
+def return_closest(point_,points_list):
+    x, y = point_[0], point_[1]
+    # gets smallest distance between point and minimum distance point
+    min_dist = min([((x-s[0])**2+(y-s[1])**2)**0.5 for s in points_list])
+    # first 
+    min_index = points_list.index([s for s in points_list if ((x-s[0])**2+(y-s[1])**2)**0.5==min_dist][0])
+    point1 = points_list[min_index]
+    points_list.remove(point1)
+    return point1
+
+def sort_CCW(points_pd):
+    ''''
+    '''
+    points_pd.columns = ['x','y']
+    points = list(zip(list(points_pd['x']),list(points_pd['y'])))
+    sorta = []
+    max_y = max(list(points_pd['y']))
+    points = list(zip(list(points_pd['x']),list(points_pd['y'])))
+    # select point with maximum y coordinate 
+    sorta.append([x for x in points if x[1]==max_y][0]) # add point with maximum y coordinate, as tuple, to sorta
+    points.remove(sorta[0]) # remove max y point from points list
+    p1,p2 = return_two_closest(sorta[0],points)
+    if p1[0]<p2[0]:   # if p1 is the leftmost point
+        sorta.append(p1) # add it to sorta
+    else:
+        sorta.append(p2)
+# Iterate through each point 
+    while len(points)>0:
+        p1 = return_closest(sorta[len(sorta)-1],points)
+        sorta.append(p1)
+    return sorta
+
+# Does this make the result folder if not present? (no)
+bit = "organoid_point_clouds"
+data_directory = "C:\\Users\\graci\\OneDrive\\Documents\\Lab\\Polar AC\\"+bit
+result_directory = "C:\\Users\\graci\\OneDrive\\Documents\\Lab\\Polar AC\\"+bit+"\\results_arc"
+csv_files = glob.glob(os.path.join(data_directory, "*.csv"))
+
+
+#Iterative processing of numbered images, starting with 1.csv
+for j in csv_files:
+    os.chdir(data_directory)
+    fileName = j.split("\\")[-1]
+    points_pd = pd.read_csv(j)
+    points_pd.columns=['x','y']
+    points_pd = pd.DataFrame(sort_CCW(points_pd))
+    points_pd.columns=['x','y']
+    points_pd['r']=0
+    points_pd['arc_length']=0
+
+    #adding points converted to polar
+
+    x_bar=np.mean(points_pd['x'])
+    y_bar=np.mean(points_pd['y'])
+    for i in range(len(points_pd)):
+        points_pd.iloc[i,2]= np.sqrt((points_pd.iloc[i,0]-x_bar)**2+(points_pd.iloc[i,1]-y_bar)**2)
+    # Adding arc length
+    arc_length(points_pd) # points_pd is now in terms of arc length 
+        
+
+    # First, points are sorted by theta from 0-360 degrees
+    # points.sort_values(by=["theta"],inplace=True,ascending=True)
+    # Then they are re-sorted such that the first point is the one with the largest radial coordiante
+    points_arc = list(points_pd['arc_length'])
+    points_r = list(points_pd['r'])
+    max_r_index = points_r.index(max(points_r))
+    points_r = points_r[max_r_index:len(points_r)]+points_r[0:max_r_index] # slices and recombines 
+    points_arc = points_arc[max_r_index:len(points_r)]+points_arc[0:max_r_index]
+    points_pd['r'] = points_r
+    points_pd['arc_length']= points_arc
+    
+    #angular remapping, sets the zero of arc length to be the max radial coordinate 
+    new_arc = []
+    for i in points_arc:
+        if i>=points_arc[0]:
+            new_arc.append(i-points_pd['arc_length'][0])
+        else:
+            new_arc.append(i+360-points_pd['arc_length'][0])
+    
+    points_pd['arc_length'] = new_arc
+    #points_arc = new_arc
+    #When do I last use/need the points DF?
+    
+    # Make list of points 
+    
+    # new_theta, points_x, points_y and points_r are lists of points
+    
+    
+    
+    
+    
+    #OLD
+    result = pd.DataFrame(columns=['arc_length','r_cor','num_features','num_features_2nd'])
+    result['arc_length']=points_pd['arc_length']
+    result['r_cor']=autocorrelate(points_pd['r'])
+    result_vector = list(result['r_cor']) # Sack up and use Pandas for the whole thing 
+    
+    # Dimensional Reduction
+    result.iloc[0,2] = int(sign_change(result_vector)/2)
+    result.iloc[0,3] = tiebreaker(result_vector)/2
+    # Dimensional Reduction by way of scipy maxima detection
+    '''
+    numpy_result = result['r_cor'].to_numpy()
+    
+    print(fileName,"maxima: ",len(argrelextrema(numpy_result, np.greater)[0]),"minima: ",len(argrelextrema(numpy_result, np.less)[0]))
+    indecies_of_maxima = argrelextrema(numpy_result, np.greater)[0]
+    indecies_of_minima = argrelextrema(numpy_result, np.less)[0]
+    num_maxima = len(indecies_of_maxima)
+    if num_maxima == 2: # This part of the control flow deals with the tiebreaker case of two features
+       num_maxima_2 = tiebreaker(numpy_result) # num maxima 2 is feature number returned by second derivative test
+       if not (num_maxima_2 == 2 or num_maxima_2 == 3):
+           result.iloc[0,2] = "inconclusive"
+       else:
+           result.iloc[0,2] = num_maxima_2
+    else:
+        result.iloc[0,2] = num_maxima
+    '''
+    # Change current directory
+    os.chdir(result_directory)
+    result.to_csv("!"+fileName[0:-4]+"_PolarAC.csv",index=False)
+    #print(fileName+" processed,", "features")
+    print(fileName+" processed,", int(sign_change(result_vector)/2), "features",tiebreaker(result_vector)/2)
+
+finishTime = time.time()
+
+print("The program took ",np.floor(finishTime-startTime)," seconds")
---
+++ PolarAC_results_processing_arc.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 28 10:35:44 2023
+
+@author: graci
+"""
+
+import numpy as np
+import pandas as pd
+import os
+import time
+import glob
+
+#get all csv result files 
+bit = "organoid_point_clouds"
+directory = "C:\\Users\\graci\\OneDrive\\Documents\\Lab\\Polar AC\\"+bit+"\\results_arc"
+csv_filepaths = glob.glob(os.path.join(directory, "*.csv")) #list of filenames/directories as strings 
+
+# filenames is av_filepaths with directory and _PolarAC trimmed off
+filenames = [x[76:-12] for x in csv_filepaths]
+# Feature numbers list stores number of features 
+feature_numbers = []
+feature_numbers2 = []
+# Result stores everything
+result = pd.DataFrame()
+print(filenames[0])
+for i,j in enumerate(csv_filepaths):
+    # read contents of csv, store individual columns 
+    points = pd.read_csv(j)
+    points.columns=[filenames[i]+' theta',filenames[i]+' r autocorrelation',filenames[i]+' number of features',filenames[i]+' number of features 2']
+    # add only theta and r_cor to final dataframe 
+    feature_numbers.append(points[filenames[i]+' number of features'][0])
+    feature_numbers2.append(points[filenames[i]+' number of features 2'][0])
+    result = pd.concat([result,points[[filenames[i]+' theta',filenames[i]+' r autocorrelation']]], axis=1)
+
+# Add two new colums for filename / feature number
+filenames_df = pd.DataFrame(filenames)
+filenames_df2 = pd.DataFrame(filenames)
+feature_num_df =  pd.DataFrame(feature_numbers)
+feature_num2_df = pd.DataFrame(feature_numbers2)
+filenames_df.columns = ["sign change"] 
+filenames_df2.columns = ["2nd deriv"] 
+feature_num_df.columns=["num features sign change"]
+feature_num2_df.columns=["num features 2nd deriv."]
+result = pd.concat([filenames_df,filenames_df2,feature_num_df,feature_num2_df,result],axis=1)
+
+#  Write new file to this directory 
+directory = "C:\\Users\\graci\\OneDrive\\Documents\\Lab\\Polar AC\\"+bit+"\\results_arc"
+os.chdir(directory)
+result.to_csv("!!!!Compiled_results_otho_meta_para.csv",index=False)
+    
+    
